f00c2eb94142d7cf997fa85f047364d6f9df6409
Mallikarjun B Y <mallikarjun09051997@gmail.com>
2026-02-20 16:53:24 +0530

    old-code

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://taskmanager-backend-1-9qi6.onrender.com/api",
   withCredentials: true
 })
 
